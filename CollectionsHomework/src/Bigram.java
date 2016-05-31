25dc1b88be5915b56650d1de14f2a1c22c42e243
popovic-marko <popovic.marko14@gmail.com>
2016-05-31 23:24:04 +0200

    Omoguceno predvidjanje za odgovarajuci string.

--- CollectionsHomework/src/Bigram.java
+++ CollectionsHomework/src/Bigram.java
@@ -4,6 +4,28 @@ import java.util.Scanner;
 
 public class Bigram {
 
+	public static String pronadjiSledeci(Map<String, Integer> mapa, String s){
+		String max = new String();
+		int n = 0;
+		for(Map.Entry<String, Integer> e : mapa.entrySet()){
+			if(e.getKey().startsWith(s) && e.getValue() > n){
+				max = e.getKey().substring(1, 2);
+				n = e.getValue();
+			}
+		}
+		return max;
+	}
+
+	public static boolean proveri(Map<String, Integer> mapa, String s){
+
+		for(Map.Entry<String, Integer> e : mapa.entrySet()){
+			if(e.getKey().endsWith(s)){
+				return true;
+			}
+		}
+		return false;
+	}
+
 	public static void main(String[] args) {
 
 		Map<String, Integer> mapa = new HashMap<>();
@@ -28,6 +50,26 @@ public class Bigram {
 
 			System.out.println(e.getKey() + " " + e.getValue());
 		}    
+
+		System.out.print("\n" + "Unesite string za predvidjanje karaktera(mora postojati u prethodnom stringu): ");
+		boolean kraj = false;
+		String novi = new String();
+		while(!kraj){
+			novi = in.nextLine();
+			if(proveri(mapa, novi.substring(novi.length() - 1))){
+				kraj = true;
+			} else {
+				System.out.println("\n" + "Uneti string ne postoji u prethodnom!");
+				System.out.print("Unesite string za predvidjanje karaktera: ");
+			}
+		}
+
+		for (int i = 0; i < 3; i++) {
+			String s = novi.substring(i + 1, i + 2);
+			novi = novi + pronadjiSledeci(mapa, s);
+		}
+
+		System.out.println("\n" + "Rezultat predvidjanaja: " + novi);
 
 	}
 }
